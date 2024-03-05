55e71af7d45afd6b439e9078d67ada8411517b01
bt-png <110639339+bt-png@users.noreply.github.com>
2024-03-05 16:41:29 +0000

    simple

--- Hello.py
+++ Hello.py
@@ -24,7 +24,7 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# :balloon: Welcome to Streamlit! 👋")
 
     st.sidebar.success("Select a demo above.")
 
