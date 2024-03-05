6c9b8173190a9d358d48954a61bcdd0f47247919
bt-png <110639339+bt-png@users.noreply.github.com>
2024-03-05 11:28:26 -0800

    Update 4_Calc1.py

--- pages/4_Calc1.py
+++ pages/4_Calc1.py
@@ -18,7 +18,7 @@ import numpy as np
 
 import streamlit as st
 from streamlit.hello.utils import show_code
-from "/mount/src/ocs/library" import system as OCS
+import system as OCS
 
 def calc1() -> None:
 
