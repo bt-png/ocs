340ee26cec139197fe09e4e1a6c10314ed5f4bd1
bt-png <110639339+bt-png@users.noreply.github.com>
2024-03-13 11:23:35 -0700

    Update system.py

--- system.py
+++ system.py
@@ -199,7 +199,8 @@ def ha_scr(_df) -> str:
         str(x) + "'," + str(z)
         for x, y, z in
         zip(df['Stationing'], df['CW Elevation'], df['MW Elevation'])])
-    val = df.to_csv(columns=['hastr'], index=False, header=False, quoting=2, encoding='UTF-8')
+    val = df.to_csv(columns=['hastr'], index=False, header=False, encoding='UTF-8')
+    val = val.strip('"')
     return val
 
 def _pad_scr(txt) -> str:
