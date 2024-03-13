bb0dc3af852b3bae86bb91f0020b993da8c0b44e
bt-png <110639339+bt-png@users.noreply.github.com>
2024-03-12 19:51:56 -0700

    Update general.py

--- general.py
+++ general.py
@@ -74,7 +74,7 @@ def sag(l,H,G,h,a):
 def x_SPAN_LIST(WR, STA_Step, STARound):
     STAStart = RoundVal(WR['STA'].iloc[0],STARound)
     STAEnd = RoundVal(WR['STA'].iloc[-1],STARound)
-    #TotalElements=int((STAEnd-STAStart)/STA_Step)
+    TotalElements=int((STAEnd-STAStart)/STA_Step)
     val = np.linspace(STAStart,STAEnd,num=TotalElements, endpoint=True)
     #val = np.arange(STAStart, STAEnd, RoundVal(STA_Step,STARound))
     val = np.unique(RoundVal(val,STARound))
