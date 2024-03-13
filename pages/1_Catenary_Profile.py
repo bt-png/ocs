de2190f13ab54d4a6e7d28b088606e1c656b79bb
bt-png <110639339+bt-png@users.noreply.github.com>
2024-03-12 23:24:05 -0700

    Update 1_Catenary_Profile.py

--- pages/1_Catenary_Profile.py
+++ pages/1_Catenary_Profile.py
@@ -291,6 +291,7 @@ with tab4:
             else:
                 OutputSag(Nom)
         with cdd2:
+            st.container()
             st.download_button(
                 label="### press to download script",
                 data=sample_wr_csv,
