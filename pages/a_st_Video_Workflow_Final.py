c9f922f0f3a4eb2481377de0be82b44267c682b1
bt-png <110639339+bt-png@users.noreply.github.com>
2024-09-23 10:41:19 -0700

    Update a_st_Video_Workflow_Final.py

--- pages/a_st_Video_Workflow_Final.py
+++ pages/a_st_Video_Workflow_Final.py
@@ -432,7 +432,8 @@ def Create_Final_Video(v_dict: dict):
 
 
 def run(video_dict):
-
+    if st.session_state['accesskey'] not in st.secrets['accesskey']:
+        st.stop()
     upload_dir = video_dict['Upload Folder']
     base_name, ext = os.path.splitext(video_dict['Video Name'])
     save_folder = os.path.join(upload_dir, base_name)
